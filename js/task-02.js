6539e7b7e597fe38a13423626cbcfa36078e562f
MykolaKurin <nvk1967@gmail.com>
2021-10-15 20:14:37 +0300

    Task 2 & 4
    2 tasks

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const itemEl = document.querySelector('#ingredients')
+const listEl = ingredients.forEach((ingredient) => {
+
+  const itemListEl = document.createElement('li')
+    itemListEl.classList.add('item')
+    itemListEl.textContent = ingredient
+    itemEl.appendChild(itemListEl)
+})
+
+  console.log(itemEl)
