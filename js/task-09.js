649f7739d3ff149425f07cc92aac9b97636c8e44
MykolaKurin <nvk1967@gmail.com>
2021-10-16 17:27:24 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const randomColor = document.querySelector(".widget");
+randomColor.addEventListener("click", onButtonClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function onButtonClick(evt) {
+  if (!evt.target.classList.contains("changeColor")) {
+  const color = getRandomHexColor();
+  document.body.style.background = color;
+  return;
+  }
+}
+
+console.log();
