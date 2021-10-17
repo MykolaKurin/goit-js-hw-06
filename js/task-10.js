9c080d08a7da873d47747d3218d204068154d251
MykolaKurin <nvk1967@gmail.com>
2021-10-17 20:35:33 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,23 @@
+const createBtnRef = document.querySelector(
+  'button[data-create]');
+  
+const destroyBtnRef = document.querySelector(
+  'button[data-destroy]');
+
+const inputRef = document.querySelector('input');
+
+createBtnRef.addEventListener("click", onCreate);
+destroyBtnRef.addEventListener("click", destroyBoxes);
+
+function onCreate() {
+  createBoxes(inputRef.value);
+}
+
+function createBoxes(amount) {
+  const boxNumberRef = 
+}
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
